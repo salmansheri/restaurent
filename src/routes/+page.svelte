c48ec3e32929a48c0702b358dca81a0941078d37
salmansheri <sheriffsalman00@gmail.com>
2023-07-25 04:18:47 +0530

    minor fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,7 @@
 	import FeaturedItems from './FeaturedItems.svelte';
 	import Offer from './Offer.svelte';
 	import Slider from './Slider.svelte';
-	import Logo from '$lib/images/svelte-logo.svg';
-	import Github from '$lib/images/github.svg';
 </script>
-
-<svelte:head>
-	<title>Restaurent - by salman sheriff</title>
-	<meta name="description" content="Restaurent Website" />
-	<link rel="icon" href={Github} />
-</svelte:head>
 
 <section>
 	<Slider />
